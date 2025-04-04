92789a60d2aa65cc0497f41c1451831db3aa04e2
Tag <ryoooma2002@gmail.com>
2025-04-04 16:47:38 +0900

    Second commit

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-var addtask = document.querySelector('#addtask');
-var form = document.querySelector('#form');
+var addtask = document.querySelector('#add-task');
+var taskname = document.querySelector('#taskname');
+var taskdescription = document.querySelector('#taskdescription');
+var taskstatus = document.querySelector('#taskstatus');
+
+let task_data = {
+    name: taskname.value,
+    detail: taskdescription.value,
+    status: taskstatus.value
+}
 
 addtask.addEventListener('click', function () { 
+    if (taskname.value !== ''){
+    localStorage.setItem(task_data.name, JSON.stringify(task_data));
+
+}
+    else{
+        alert('空白です。');
+    }
+ }, false);
+
+window.addEventListener('load', function(){
+
     var list = document.getElementById('list');
 
     var li = document.createElement('li');
 
-    li.innerHTML = form.value;
-
+    let tasks = JSON.parse(localStorage.getItem('rrfee'));
+    
+    li.innerHTML = tasks;
+    
     list.appendChild(li);
- }, false);
+})
 
 
- if (!window.localStorage) {
-    console.log("localstorage非対応");
-}
 
 
 
